2b0a00c0f7daf5da7a2c7fc4dff54390708a1778
Bzita <banszegi.zita@gmail.com>
2018-02-16 11:36:26 +0100

    ExerciseNumber18

--- week-01/day-04/src/Exercise7.java
+++ week-01/day-04/src/Exercise7.java
@@ -1,14 +1,24 @@
+import java.util.Scanner;
+
 public class Exercise7 {
 
     public static void main(String[] args){
 
-        int h = 135798745;
-        // tell if it has 11 as a divisor (print as a boolean)
+        // 18 - OddEven
 
-        //System.out.println(h % 11);
-        Boolean compare;
-        compare = (h % 11) == 0;
-        System.out.println(compare);
+        // Write a program that reads a number from the standard input,
+        // Then prints "Odd" if the number is odd, or "Even" if it is even.
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println ("Please give an integer, then press Enter. I will tell if it is odd or even.");
+        int userInput = scanner.nextInt();
+
+        if (userInput % 2 == 0){
+            System.out.println("Even.");
+        }
+        if (userInput % 2 == 1){
+            System.out.println("Odd.");
+        }
 
     }
 }
