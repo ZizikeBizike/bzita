e5a3a5c558dc742da3edbd68cfd8e67aa672c210
Bzita <banszegi.zita@gmail.com>
2018-02-16 14:11:53 +0100

    exerciseNumber23and24

---
+++ week-01/day-04/src/ExerciseNumber23.java
@@ -0,0 +1,15 @@
+public class ExerciseNumber23 {
+
+    public static void main(String[] args) {
+
+        // 23 - IWontCheatOnTheExam
+
+        // Create a program that writes this line 100 times:
+        // "I won't cheat on the exam!"
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println("I won't cheat on the exam!");
+        }
+
+    }
+}
