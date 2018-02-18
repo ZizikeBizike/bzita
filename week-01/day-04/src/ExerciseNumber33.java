2331ffb785c9f556a96d210f820176be18f4c6b7
Bzita <banszegi.zita@gmail.com>
2018-02-18 20:07:17 +0100

    ExerciseNumber33

--- week-01/day-04/src/ExerciseNumber33.java
+++ week-01/day-04/src/ExerciseNumber33.java
@@ -1,1 +1,34 @@
+import java.util.Scanner;
 
+public class ExerciseNumber33 {
+    public static void main(String[] args) {
+
+        // 33 - GuessTheNumber
+
+        // Write a program that stores a number, and the user has to figure it out.
+        // The user can input guesses, after each guess the program would tell one
+        // of the following:
+        //
+        // The stored number is higher
+        // The stried number is lower
+        // You found the number: 8
+
+        Scanner scanner = new Scanner(System.in);
+        int userInput;
+        int numberToGuess = 8;
+        System.out.println("I thought of an integer between 0 and 100.");
+        do {
+            System.out.println("Guess the number!");
+            userInput = scanner.nextInt();
+            if (userInput == numberToGuess) {
+                System.out.println("You found the number: 8");
+            } else if (userInput < 0 || userInput > 100) {
+                System.out.println("The number is between 0 and 100.");
+            } else if (userInput < numberToGuess && userInput >= 0) {
+                System.out.println("The stored number is higher.");
+            } else if (userInput > numberToGuess && userInput <= 100) {
+                System.out.println("The stored number is lower.");
+            }
+        } while (userInput != numberToGuess);
+    }
+}
