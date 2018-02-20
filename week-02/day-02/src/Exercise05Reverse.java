6865598849376ea8ec7202fed55fe66da40dfdfe
Bzita <banszegi.zita@gmail.com>
2018-02-20 14:50:12 +0100

    Exercise05Reverse

---
+++ week-02/day-02/src/Exercise05Reverse.java
@@ -0,0 +1,20 @@
+public class Exercise05Reverse {
+
+    public static void main(String... args){
+        String reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
+
+        // Create a method that can reverse a String, which is passed as the parameter
+        // Use it on this reversed string to check it!
+        // Try to solve this using charAt() first, and optionally anything else after.
+        System.out.println(reverse(reversed));
+    }
+
+    private static String reverse(String reversed) {
+        String reversedFinal = "";
+
+        for (int i = reversed.length() - 1; i > 0; i = i - 1) {
+            reversedFinal += reversed.charAt(i);
+        }
+        return reversedFinal;
+    }
+}
