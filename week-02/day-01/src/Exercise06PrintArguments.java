8cf9e7536649d462306cea76f392bca22f042c84
Bzita <banszegi.zita@gmail.com>
2018-02-20 12:11:41 +0100

    Some exercises

---
+++ week-02/day-01/src/Exercise06PrintArguments.java
@@ -0,0 +1,27 @@
+public class Exercise06PrintArguments {
+
+    //  Create the usual class wrapper
+    //  and main method on your own.
+
+    // - Create a function called `printer`
+    //   which prints the input String parameters
+    // - It can have any number of parameters
+
+    // Examples
+    // printer("first")
+    // printer("first", "second")
+    // printer("first", "second", "third", "fourh")
+    // ...
+
+    public static void main(String[] args){
+
+        printer("first","second","third","fourth");
+    }
+
+    public static void printer(String... s){
+        for (int i = 0; i < s.length; i++) {
+            System.out.println(s[i]);
+        }
+
+    }
+}
