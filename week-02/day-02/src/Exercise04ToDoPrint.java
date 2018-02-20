c240dc8507a41115c82177bfd82814a7bd38485e
Bzita <banszegi.zita@gmail.com>
2018-02-20 14:16:56 +0100

    Exercise04ToDoPrint

---
+++ week-02/day-02/src/Exercise04ToDoPrint.java
@@ -0,0 +1,21 @@
+public class Exercise04ToDoPrint {
+
+    public static void main(String... args){
+        String todoText = " - Buy milk\n";
+        // Add "My todo:" to the beginning of the todoText
+        // Add " - Download games" to the end of the todoText
+        // Add " - Diablo" to the end of the todoText but with indention
+
+        // Expected outpt:
+
+        // My todo:
+        //  - Buy milk
+        //  - Download games
+        //      - Diablo
+
+        String MyToDo = "My todo:\n";
+        todoText = MyToDo + todoText.concat(" - Download games\n").concat("    - Diablo");
+
+        System.out.println(todoText);
+    }
+}
