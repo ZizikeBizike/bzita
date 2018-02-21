6c4d1f5095eb8541919edb5ce890dd1038c19d52
Bzita <banszegi.zita@gmail.com>
2018-02-21 11:34:28 +0100

    Exercise05Horizontal

---
+++ week-02/day-03/src/Exercise05Horizontal.java
@@ -0,0 +1,50 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class Exercise05Horizontal {
+    public static void mainDraw(Graphics graphics) {
+        // Create a line drawing function that takes 2 parameters:
+        // The x and y coordinates of the line's starting point
+        // and draws a 50 long horizontal line from that point.
+        // Draw 3 lines with that function. Use loop for that.
+
+        int x = 50;
+        int y = 50;
+
+        for (int i =0; i < 3; i++){
+            y = y + 50;
+            drawingMethod(graphics, x, y);
+        }
+
+    }
+
+    public static void drawingMethod(Graphics graphics, int x, int y){
+
+        graphics.drawLine(x, y, x + 50, y);
+
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 343;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        jFrame.add(new ImagePanel());
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
