d32dd103573631a5c235e9993b5bd564007fef8e
Bzita <banszegi.zita@gmail.com>
2018-02-22 12:57:04 +0100

    Exercise16AppendA

--- week-02/day-01/src/Exercise16AppendA.java
+++ week-02/day-01/src/Exercise16AppendA.java
@@ -1,4 +1,15 @@
 public class Exercise16AppendA {
 
+// - Create an array variable named `nimals`
+//   with the following content: `["kuty", "macsk", "cic"]`
+// - Add all elements an `"a"` at the end
 
+    public static void main(String[] args){
+
+        String[] nimals = {"kuty", "macsk", "cic"};
+        for (int i = 0; i < nimals.length; i++) {
+            nimals[i] = nimals[i] + "a";
+            System.out.print(nimals[i] + ", ");
+        }
+    }
 }
