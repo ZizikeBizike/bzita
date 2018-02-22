64c4fd599e6da0dfa6e88e8d2005088fb97a2c55
Bzita <banszegi.zita@gmail.com>
2018-02-22 16:27:50 +0100

    little changes

--- week-02/day-02/src/Exercise03TakesLonger.java
+++ week-02/day-02/src/Exercise03TakesLonger.java
@@ -7,15 +7,9 @@ public class Exercise03TakesLonger {
         // Add "always takes longer than" to the StringBuilder (quote) between the words "It" and "you"
         // Using pieces of the quote variable (instead of just redefining the string)
 
-        quote = quote.replace("It you", "It always takes longer than you");
+        String missingPart = "always takes longer than ";
+        quote = quote.substring(0, 21) + missingPart + quote.substring(21,81);
+
         System.out.println(quote);
-
-
-        String quote2 = "Hofstadter's Law: It you expect, even when you take into account Hofstadter's Law.";
-        String substring1 = quote2.substring(0, 20);
-        String substring2 = quote2.substring(20);
-        String toAdd = " always takes longer than";
-        quote2 = substring1 + toAdd + substring2;
-        System.out.println(quote2);
     }
 }
