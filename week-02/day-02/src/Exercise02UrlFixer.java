3550023925c322e3cf6562313a82e0d1b38b7e8b
Bzita <banszegi.zita@gmail.com>
2018-02-23 15:32:30 +0100

    little changes

--- week-02/day-02/src/Exercise02UrlFixer.java
+++ week-02/day-02/src/Exercise02UrlFixer.java
@@ -2,7 +2,6 @@ public class Exercise02UrlFixer {
 
     public static void main(String... args){
 
-
         // Accidentally I got the wrong URL for a funny subreddit. It's probably "odds" and not "bots"
         // Also, the URL is missing a crutial component, find out what it is and insert it too!
         // Try to solve it more than once using different String functions!
@@ -11,16 +10,12 @@ public class Exercise02UrlFixer {
         String url = "https//www.reddit.com/r/nevertellmethebots";
 
         url = url.replace ("bots", "odds");
-        String substring1 = url.substring(0, 5);
-        String substring2 = url.substring(5);
         String toAdd = ":";
-
-        substring1 = substring1 + toAdd;
-        url = substring1 + substring2;
+        url = url.substring(0, 5) + toAdd + url.substring(5);
 
         System.out.println(url);
 
-
+        //Easier version:
 
         String url2 = "https//www.reddit.com/r/nevertellmethebots";
 
