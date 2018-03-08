4631f825569646ba836e9515816aca6bf0ab5d7f
Bzita <banszegi.zita@gmail.com>
2018-03-08 13:16:37 +0100

    feat(Apple) task is done

---
+++ week-04/day-04/src/Sum.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class Sum {
+
+  public static void main(String[] args) {
+    ArrayList<Integer> sum = new ArrayList<Integer>();
+    sum.add(119);
+    sum.add(57);
+    sum.add(7);
+    sum.add(38);
+    sum.add(8);
+    sum.add(11);
+
+    System.out.println(sumOfNumbers(sum));
+  }
+
+  public static int sumOfNumbers(ArrayList<Integer> inputNumbers){
+    int result = 0;
+    for(int i = 0; i < inputNumbers.size(); i++){
+      result += inputNumbers.get(i);
+    }
+    return result;
+  }
+}
