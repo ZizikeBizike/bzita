35a5f12b091fc9f662152e4e8a7ed773f1b44525
Bzita <banszegi.zita@gmail.com>
2018-04-03 16:02:31 +0200

    feat(Hello World Web App) task is done

--- week-07/day-02/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week-07/day-02/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloRESTController {
 
   @RequestMapping("/greeting")
-
   public Greeting greetingMethod() {
     return new Greeting(1, "Hello World!");
   }
