e92845cc292b71813e5f2949371302fa661c9dca
Bzita <banszegi.zita@gmail.com>
2018-04-17 16:34:55 +0200

    feat(Groot) tests are ready

--- week-09/day-02/src/test/java/com/greenfoxacademy/resttest/controller/GardianControllerTest.java
+++ week-09/day-02/src/test/java/com/greenfoxacademy/resttest/controller/GardianControllerTest.java
@@ -1,12 +1,61 @@
 package com.greenfoxacademy.resttest.controller;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import java.nio.charset.Charset;
 
 import static org.junit.Assert.*;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@WebAppConfiguration
+@EnableWebMvc
 public class GardianControllerTest {
 
+  private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+          MediaType.APPLICATION_JSON.getSubtype(),
+          Charset.forName("utf8"));
+
+  private MockMvc mockMvc;
+
+  @Autowired
+  private WebApplicationContext webApplicationContext;
+
+  @Before
+  public void setup() throws Exception {
+    this.mockMvc = webAppContextSetup(webApplicationContext).build();
+  }
+
   @Test
-  public void groot() {
+  public void grootTestWithMessage() throws Exception {
+    mockMvc
+            .perform(get("/groot?message=haliho")
+                    .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.received").value("haliho"))
+            .andExpect(jsonPath("$.translated").value("I am Groot!"));
+  }
+
+  @Test
+  public void grootTestWithoutMessage() throws Exception {
+    mockMvc
+            .perform(get("/groot")
+                    .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.error").value("I am Groot!"));
   }
 }
