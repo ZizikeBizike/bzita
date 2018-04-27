f135728a8dde7644927e8f30951214fa97e8338c
Bzita <banszegi.zita@gmail.com>
2018-04-27 15:54:26 +0200

    feat(practice) it works! :-)

--- retrofitPractice/app/src/main/java/com/greenfoxacademy/retrofitpractice/ApiInterface.java
+++ retrofitPractice/app/src/main/java/com/greenfoxacademy/retrofitpractice/ApiInterface.java
@@ -1,12 +1,11 @@
 package com.greenfoxacademy.retrofitpractice;
 
 import java.util.List;
-
 import retrofit2.Call;
-import retrofit2.http.POST;
+import retrofit2.http.GET;
 
 public interface ApiInterface {
 
-    @POST("/posts/1/comments")
+    @GET("/posts/1/comments")
     Call<List<Contact>> getContacts();
 }
